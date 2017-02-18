084ffacf41302856f6d87cd35970fea931199ec0
shellshock1953 <shellshock1953@gmail.com>
2017-02-18 22:50:36 +0200

    first comm

---
+++ check_ping.sh
@@ -0,0 +1,9 @@
+ping -c 1 8.8.8.8 > /dev/null 2>&1
+if [[ "$?" -ne 0 ]]; then
+	echo "NO_NET"
+	exit 1
+fi
+if [[ "$?" -eq 0 ]]; then
+	MS=`ping -c 1 8.8.8.8 | head -2 | tail -1 | cut -d'=' -f4-`
+	echo $MS
+fi
