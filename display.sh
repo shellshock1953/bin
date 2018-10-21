bffbe02f09e1ccf0613873bdbec64b6e7b55fa2d
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2018-10-21 18:01:58 +0300

    display update res

--- display.sh
+++ display.sh
@@ -5,5 +5,5 @@
 #xrandr --output VGA-1 --mode "1920x1080_60.00"
 
 xrandr --newmode "1920x1200_60.00"  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
-xrandr --addmode VGA-1 "1920x1200_60.00"
-xrandr --output VGA-1 --mode "1920x1200_60.00"
+xrandr --addmode DP1 "1920x1200_60.00"
+xrandr --output DP1 --mode "1920x1200_60.00"
